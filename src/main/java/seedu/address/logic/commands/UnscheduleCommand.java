f23b057b821af56feacd59f0ec1cc5caf78559cc
Md Azhar <md.azsa@gmail.com>
2018-04-09 10:21:51 +0800

    Adds the UnscheduleCommand

---
+++ src/main/java/seedu/address/logic/commands/UnscheduleCommand.java
@@ -0,0 +1,7 @@
+package seedu.address.logic.commands;
+
+/**
+ * Deletes an appointment identifiedusing it's last displayed index from the program.
+ */
+public class UnscheduleCommand extends UndoableCommand {
+}
